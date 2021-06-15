6716595382f869f0cfb5ed6ebfc651e47430bcc7
Tim Pustovit <tim.pustovit@gmail.com>
2021-06-15 13:01:19 +1000

    console works

--- Assets/Scripts/consoleController.cs
+++ Assets/Scripts/consoleController.cs
@@ -1,25 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class consoleController : MonoBehaviour
 {
     public TMP_InputField inputField;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TMP_Text consoleLog;
+    int i = 0;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        inputField.ActivateInputField();    // activates the inputfield
     }
 
     public void sendit()
     {
-        Debug.Log("aaaaaaaaaaaa");
+        Debug.Log("get text " + i);
+        getit();
+        i++;
+    }
+
+    public void getit()
+    {
+        string txt = inputField.text;
+        string log = consoleLog.text;
+        string x = log + "\n>> " + txt;
+        consoleLog.text = x;
+        Debug.Log(txt);
     }
 }

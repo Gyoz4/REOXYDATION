28b3bafb7990738f2aee2d8554258b8d6bfd05db
Tim Pustovit <tim.pustovit@gmail.com>
2021-09-08 13:41:54 +1000

    1.01.1 tutorial
    updated tutorial

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -19,6 +19,8 @@ public class SceneChanger : MonoBehaviour
     {
 		Screen.fullScreen = true;
 		Settings.SetActive(false);
+		Tutorial.SetActive(false);
+		MainMenu.SetActive(true);
 	}
 
 	public void ChangeScene(string sceneName)
